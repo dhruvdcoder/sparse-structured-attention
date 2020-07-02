85ce47d7344b1a7d3850a68001b12978d7f6d288
Dhruvesh <dhruvdplayer@gmail.com>
2020-07-02 11:43:00 -0400

    Update sparsemax.py

--- torchsparseattn/sparsemax.py
+++ torchsparseattn/sparsemax.py
@@ -18,7 +18,7 @@ from torch.autograd import Function
 def project_simplex(v, z=1):
     v_sorted, _ = torch.sort(v, dim=0, descending=True)
     cssv = torch.cumsum(v_sorted, dim=0) - z
-    ind = torch.arange(1, 1 + len(v)).to(dtype=v.dtype)
+    ind = torch.arange(1, 1 + len(v)).to(dtype=v.dtype, device=v.device)
     cond = v_sorted - cssv / ind > 0
     rho = ind.masked_select(cond)[-1]
     tau = cssv.masked_select(cond)[-1] / rho
